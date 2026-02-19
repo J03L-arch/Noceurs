015769eaca181ad05ab12cccb387cca3f255a944
# nziokimutisya54-commit <nziokimutisya58@gmail.com>
2026-02-19 17:34:18 +0300

    mike.py

---
+++ mike.py
@@ -0,0 +1,14 @@
+# Benedict musa
+# 18/2/2026
+# group work
+
+name = "Benard musa"
+fav_sport = "football and basketball"
+grade = "B+"
+fav_hobby = "play video games"
+fav_food = "junk food"
+
+print(f"M name is {name}")
+print(f"My fav_sport is {fav_sport}")
+print(f"My fav_hobby is {fav_hobby}")
+print(f"My fav_food is {fav_food}")
