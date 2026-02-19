420c9a61424694a68b85bc5f427b7f21a21b5abf
kimmy254173 <gitaukimberley@gmal.com>
2026-02-19 11:12:39 +0300

    Kimberley,student

---
+++ Kimberley/Kimberley.py
@@ -0,0 +1,13 @@
+# Name : Kimberley Gitau
+# Date : 18/02/2026
+# Program to write details
+
+fav_sport = "Swimming"
+fav_music = "Afrobeats"
+fav_hobby = "Sleeping"
+fav_food = "Pizza"
+
+print(f"My fav_sport is {fav_sport}")
+print(f"My fav_music is {fav_music}")
+print(f"My fav_hobby is {fav_hobby}")
+print(f"My fav_food is {fav_food}")
