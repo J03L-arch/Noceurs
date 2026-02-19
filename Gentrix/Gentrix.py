f5590b7966c19cf33e8de12f401dcd7412e3576d
GENTRIX08 <aluochgentrix008@gmail.com>
2026-02-19 11:19:54 +0300

    Gentrix,student

---
+++ Gentrix/Gentrix.py
@@ -0,0 +1,13 @@
+#Name : Gentrix Anyango
+# Date : 18/02/2026
+# program to write details
+
+fav_sport = "tennis"
+fav_music = "indie"
+fav_hobby = "reading"
+fav_food = "fast_food"
+
+print(f" My fav sport is {fav_sport}")
+print(f" My fav music is {fav_music}")
+print(f" My fav hobby is {fav_hobby}")
+print(f" My fav food is {fav_food}")
