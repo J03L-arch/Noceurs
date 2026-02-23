b371cd3a33e2f53ae2a8d0fa65f16c96da075307
steve lawrnece <mainasteven328@gmail.com>
2026-02-23 11:50:56 +0300

    steve,students

---
+++ steve/steve.py
@@ -0,0 +1,10 @@
+# name : steve lawrence
+# date : 19.02.2026
+#Program:Personal details
+
+fav_snack= "puh"
+fav_sport= "football"
+fav_music= "type shii"
+fav_hobby= "cracking puh"
+
+print(f"I am Joel and i love {fav_sport}. My favourite snack is {fav_snack} and i also enjoy listening to {fav_music}. My hobby is {fav_hobby}")
