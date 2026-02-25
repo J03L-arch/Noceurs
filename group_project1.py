37e0cb2036436e4c3312f0fc9b082e7714b5552c
J03L-arch <joelmwega3041@gmail.com>
2026-02-25 11:01:40 +0300

    school management

--- group_project1.py
+++ group_project1.py
@@ -22,10 +22,51 @@ class Student():
 
 students= []
 s1= Student("Joe", "12345", "0720374671", "Computer Science")
+s2= Student("Unc", "13245", "079876543", "Business")
+s3= Student("Kim", "14235", "0723456789", "Engineering")
+
 students.append(s1)
+students.append(s2)
+students.append(s3)
 
 for student in students:
     student.display()
+
+while True:
+    print("\n---School Management System---")
+    print("1.Add Student")
+    print("2.View Students")
+    print("3.Exit")
+
+    choice= input("Enter your choice: ")
+    
+    if choice== "1":
+        name= input("Enter student name: ")
+        id_number= input("Enter student ID number: ")
+        course= input("Enter course: ")
+
+        student= Student(name, id_number, course)
+        students.append(student)
+
+        print("Student added successfully!")
+        
+    elif choice=="2":
+        if len(students)==0:
+            print("No students found")
+        else:
+            for student in students:
+                student.display()
+
+    elif choice== "3":
+        print("Exiting program...")
+        break
+
+    else:
+        print("Invalid choice. Try again")
+
+
+
+
 
 
 
