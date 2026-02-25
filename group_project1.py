b997efb88a4b09279873ea1efd086612f5357f96
J03L-arch <joelmwega3041@gmail.com>
2026-02-25 09:34:19 +0300

    school managemnet system

--- group_project1.py
+++ group_project1.py
@@ -13,27 +13,21 @@ class Student():
     def change_course(self, new_course):
         self.course= new_course
 
-student_1= Student("Joe", 12345, 254676767, "Architecture")
-student_2= Student("Steve", 67890, 254696969, "Agriculture")
-student_3= Student("Kimberly", 54321, 254020202, "Computer Science")
-student_4= Student("Trixi", 90876, 254777777, "Filming")
-student_5= Student("Bena", 10293, 254333333, "Data Science")
-
 #Display students
-def display_info(self):
-    print("Name:", self.name)
-    print("ID:", self.id_number)
-    print("Phone number:", self.phone_number)
-    print("Course:", self.course)
+    def display(self):
+        print("Name:", self.name)
+        print("ID:", self.id_number)
+        print("Phone number:", self.phone_number)
+        print("Course:", self.course)
 
-display_info(student_1)
+students= []
+s1= Student("Joe", "12345", "0720374671", "Computer Science")
+students.append(s1)
 
-#Assign new courses
-student_1.change_course("Civil Engineering")
-student_2.change_course("Dehorning")
-student_3.change_course("Data Science")
-student_4.change_course("Music production")
-student_5.change_course("Computer Science")
+for student in students:
+    student.display()
 
-display_info(student_4)
+
+
+
 
