94fbbbb8cac7622138b8ccba4b1509a154ddb708
J03L-arch <joelmwega3041@gmail.com>
2026-02-28 17:04:16 +0300

    Group project

--- group_project1.py
+++ group_project1.py
@@ -19,8 +19,19 @@ class Student():
         print("ID:", self.id_number)
         print("Phone number:", self.phone_number)
         print("Course:", self.course)
+        print("_______________________________________________")
+import os
 
 students= []
+filename="students.txt"
+
+if os.path.exists(filename):
+    with open(filename, "r") as f:
+        next(f)
+        for line in f:
+            name,phone_number,id_number,course= line.strip().split(",")
+            students.append(Student(name, id_number, phone_number, course))
+
 s1= Student("Joe", "12345", "0720374671", "Computer Science")
 s2= Student("Unc", "13245", "079876543", "Business")
 s3= Student("Kim", "14235", "0723456789", "Engineering")
@@ -36,16 +47,21 @@ while True:
     print("\n---School Management System---")
     print("1.Add Student")
     print("2.View Students")
-    print("3.Exit")
+    print("3.Change Student Course")
+    print("4.Export Students to File")
+    print("5.Delete Student")
+    print("6.Exit")
+    
 
     choice= input("Enter your choice: ")
     
     if choice== "1":
         name= input("Enter student name: ")
         id_number= input("Enter student ID number: ")
+        phone_number= input("Enter phone number: ")
         course= input("Enter course: ")
 
-        student= Student(name, id_number, course)
+        student= Student(name, id_number, phone_number, course)
         students.append(student)
 
         print("Student added successfully!")
@@ -56,8 +72,49 @@ while True:
         else:
             for student in students:
                 student.display()
+    
+    elif choice == "3":
+        search_name= input("Enter the name of student to student to update: ")
+        found= False
+        for student in students:
+            if student.name== search_name:
+                new_course= input(f"Enter new course for {student.name}: ")
+                student.change_course(new_course)
+                print(f"{student.name}'s course has been updated to {new_course}!")
+                found= True
+                break
+            if not found:
+                print(f"No student found with the name {search_name}.")
 
-    elif choice== "3":
+
+    elif choice== "4":
+        if len(students)== 0:
+            print("No student to export")
+        else:
+            filename= input("Enter file name to save(eg., students.txt):")
+            with open(filename, "w") as f:
+                f.write("name, phone number, id number, course\n")
+
+                for student in students:
+                    f.write(f"{student.name}, {student.id_number}, {student.phone_number} {student.course}\n")
+
+            print(f"All current students have been exported to {filename}")
+
+    elif choice=="5":
+        delete_name= input("Enter student name to delete: ")
+        found= False
+
+        for student in students:
+            if student.name== delete_name:
+                students.remove(student)
+                print(f"{delete_name} has been removed from the system.")
+                found= True
+                break
+
+        if not found:
+            print("Student not found")
+
+    elif choice== "6":
         print("Exiting program...")
         break
 
